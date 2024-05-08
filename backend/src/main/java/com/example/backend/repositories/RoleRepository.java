167a285973837cdfd07a9f220850555f35db75f2
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-05-08 17:14:46 +0700

    change init project

--- backend/src/main/java/com/example/backend/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.entities.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
-}
