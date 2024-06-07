32bc5b40fa2df8ebd9edcb1663a07f17281d515f
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-06-08 05:56:41 +0700

    update loggerEvents

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from 'react';
 import { me } from '@/store/slices/authSlice';
 import { dispatch } from '@/store';
-import { channel } from '@/helpers/pusherConfig';
-import { addLogger } from '@/store/slices/loggerSlice';
+// import { channel } from '@/helpers/pusherConfig';
+// import { addLogger } from '@/store/slices/loggerSlice';
 
 const App = ({ children }: { children: React.ReactNode }) => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -16,9 +16,9 @@ const App = ({ children }: { children: React.ReactNode }) => {
     return null;
   }
 
-  channel.bind('loggerEnvent', function (data: any) {
-    dispatch(addLogger(data));
-  });
+  // channel.bind('loggerEnvent', function (data: any) {
+  //   dispatch(addLogger(data));
+  // });
 
   return children;
 };
