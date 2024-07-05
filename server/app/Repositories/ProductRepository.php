d498e889fd8cf8fdf0a82db43f722e761f16ae7c
phuclehehe <103525830+phuclehehe@users.noreply.github.com>
2024-07-05 17:03:36 +0700

    update

--- server/app/Repositories/ProductRepository.php
+++ server/app/Repositories/ProductRepository.php
@@ -29,35 +29,7 @@ class ProductRepository implements ProductRepositoryInterface
         } else {
             $product = $product->paginate($perPage);
         }
-        // $product = $product->leftJoin('product_import_details', 'products.id', '=', 'product_import_details.id_product')
-        //     ->leftJoin('product_imports', 'product_import_details.import_id', '=', 'product_imports.id')
-        //     ->select(
-        //         'products.*',
-        //         DB::raw('IFNULL(SUM(CASE WHEN product_imports.status = "A" THEN product_import_details.quantity ELSE 0 END), 0) as total_quantity'),
-        //         DB::raw('IFNULL(SUM(CASE WHEN product_imports.status = "A" THEN product_import_details.quantity * product_import_details.cost_price ELSE 0 END), 0) as total_cost')
-        //     )
-        //     ->groupBy('products.id', 'products.name', 'products.image', 'products.selling_price', 'products.active', 'products.id_type', 'products.quantity')
-        //     ->orderBy(DB::raw('MAX(product_imports.receive_time)'), 'desc');
 
-        // foreach ($products as $product) {
-        //     // Cập nhật số lượng sản phẩm
-        //     $product->quantity = $product->total_quantity;
-
-        //     // Truy vấn để lấy giá bán từ phiếu nhập có thời gian sớm nhất
-        //     $lastestSellingPrice = ProductImportDetail::query()
-        //         ->join('product_imports', 'product_import_details.import_id', '=', 'product_imports.id')
-        //         ->where('product_import_details.id_product', $product->id)
-        //         ->orderBy('product_imports.receive_time', 'desc')
-        //         ->value('product_import_details.selling_price');
-
-        //     // Cập nhật giá bán từ phiếu nhập có thời gian sớm nhất
-        //     if ($lastestSellingPrice !== null) {
-        //         $product->selling_price = $lastestSellingPrice;
-        //     }
-
-
-        //     $product->save();
-        // }
         return new CollectionCustom($product);
     }
     public function getAllProduct()
