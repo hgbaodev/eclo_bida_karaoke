fa474097d4fd41fc3cbd61f8247c54f8012a704d
Kien-Huy <113742104+Kien-Huy@users.noreply.github.com>
2024-07-09 09:30:52 +0700

    fix conflict

--- client/src/app/admin/(hydrogen)/attendance/page.tsx
+++ client/src/app/admin/(hydrogen)/attendance/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import AttendanceTable from '@/app/shared/attendance';
-import CreateAttendance  from '@/app/shared/attendance/create-attendance';
-import CreateDayOff  from '@/app/shared/dayoff/create-dayoff';
+import CreateAttendance from '@/app/shared/attendance/create-attendance';
+import CreateDayOff from '@/app/shared/dayoff/create-dayoff';
 import ModalButton from '@/app/shared/modal-button';
 import PageHeader from '@/app/shared/page-header';
 const pageHeader = {
@@ -20,11 +20,11 @@ const pageHeader = {
 export default function BlankPage() {
   return (
     <>
-      <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb} >
+      <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb}>
         <ModalButton label="Attendance" view={<CreateAttendance />} customSize="600px" className="mt-0" />
         <ModalButton label="Day Off" view={<CreateDayOff />} customSize="600px" className="mt-0" />
       </PageHeader>
       <AttendanceTable />
     </>
-  );  
+  );
 }
--- client/src/utils/validators/shift-user-detail/create-shift-user-detail.ts
+++ client/src/utils/validators/shift-user-detail/create-shift-user-detail.ts
@@ -7,9 +7,6 @@ export const createShiftUserDetailSchema = z.object({
   shift: z.string().min(1, { message: messages.fullNameIsRequired }),
   day_of_week: z.string().min(1, { message: messages.fullNameIsRequired }),
   staff: z.string().min(1, { message: messages.fullNameIsRequired }),
-  status:z.string().min(1, { message: messages.statusIsRequired }),
-  day_off:z.string().min(1, { message: messages.DayoffIsRequired }),
-  
 });
 
 // generate form types from zod validation schema
