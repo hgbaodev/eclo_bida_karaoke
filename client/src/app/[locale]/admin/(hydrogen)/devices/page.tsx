7a374262ab9765641b62ec56a17f88b636bc0918
Phong <ngt.phongg@gmail.com>
2024-07-09 14:57:31 +0700

    multi languages

--- client/src/app/[locale]/admin/(hydrogen)/devices/page.tsx
+++ client/src/app/[locale]/admin/(hydrogen)/devices/page.tsx
@@ -3,6 +3,7 @@ import PageHeader from '@/app/[locale]/shared/page-header';
 import ModalButton from '@/app/[locale]/shared/modal-button';
 import DevicesTable from '@/app/[locale]/shared/devices/devices-table';
 import CreateDevice from '@/app/[locale]/shared/devices/create-device';
+import { useTranslations } from 'next-intl';
 
 const pageHeader = {
   title: 'Devices',
@@ -18,8 +19,10 @@ const pageHeader = {
 };
 
 export default function BlankPage() {
+  const t = useTranslations('Index');
   return (
     <>
+      <h1>{t('title')}</h1>
       <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb}>
         <ModalButton label="Add New Device" view={<CreateDevice />} customSize="600px" className="mt-0" />
       </PageHeader>
